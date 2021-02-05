9605c555558c569dcc751fda9fd739ca5d93501e
Vasilis <vasilis6103@gmail.com>
2021-02-05 11:00:26 +0200

    First FT commit with unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox(executable_path=r'C:\Users\user\Documents\Python Scripts\TDD\tddenv\Lib\site-packages\geckodriver.exe')
-browser.get("http://localhost:8000/")
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox(executable_path=r'C:\Users\user\Documents\Python Scripts\TDD\tddenv\Lib\site-packages\geckodriver.exe')
 
-assert "Django" in browser.title
+    def tearDown(self):
+        self.browser.quit()
 
+    def test_random(self):
+        self.browser.get("http://localhost:8000/")
+        assert "To-do" in self.browser.title,"Browser title was " + self.browser.title
+
+
+
+
+if __name__ == "__main__" :
+    # visitor = NewVisitorTest()
+    unittest.main(warnings='ignore')        #we call unittest.main() which launches the unittest test runner which automatically finds all test classes and methods and runs them
 
