6e6179e1dc47348265b17280eb1393f0657088e7
Vskouteris <vasilis6103@gmail.com>
2021-02-10 21:19:51 +0200

    Adding os to functional_test.py for finding path in different computers

--- functional_tests.py
+++ functional_tests.py
@@ -1,17 +1,19 @@
 from selenium import webdriver
 import unittest
+import os
 
 class NewVisitorTest(unittest.TestCase):
     
     def setUp(self):
-        self.browser = webdriver.Firefox(executable_path=r'C:\Users\user\Documents\Python Scripts\TDD\tddenv\Lib\site-packages\geckodriver.exe')
+        path =os.getcwd()+ r'\tddenv\Lib\site-packages\geckodriver.exe'
+        self.browser = webdriver.Firefox(executable_path=path)
 
     def tearDown(self):
         self.browser.quit()
 
     def test_random(self):
         self.browser.get("http://localhost:8000/")
-        assert "To-do" in self.browser.title,"Browser title was " + self.browser.title
+        assert "To-do" in self.browser.title,"Browser title was " + self.browser.title    
 
 
 
