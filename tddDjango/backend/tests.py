a823b7a4bb297e177059768155534e1a96bb53d6
Vasilis <vasilis6103@gmail.com>
2021-02-11 13:05:56 +0200

    adding test :def test_home_page_view_return_correct_html(self)

--- tddDjango/backend/tests.py
+++ tddDjango/backend/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
 from backend.views import *
 
 # Create your tests here.
@@ -8,3 +9,10 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func,home_page)
 
+    def test_home_page_view_return_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf-8')
+        self.assertTrue(html.startswith('<html>') or html.startswith('<!DOCTYPE html>') ,msg = f"html starts with : {html[:10]}")
+        self.assertIn('<title>Home Page</title>',html,msg=f"THE TTITLT INSIDE HTML IS DIFFERENT from THE ASSERT TEST")
+        self.assertTrue(html.endswith('</html>'),msg = f"html ends with : {html[-10:]}")
