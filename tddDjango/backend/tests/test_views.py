5503df854f891c701d0645824ad8e8c41a3ab8bf
vasilis <vasilis6103@gmail.com>
2021-03-16 09:40:12 -0700

    tests for offerViewList

--- tddDjango/backend/tests/test_views.py
+++ tddDjango/backend/tests/test_views.py
@@ -9,12 +9,7 @@ from backend.serializers import OfferSerializer,ParameterSerializer,DetailSerial
 
 @tag('views')
 class TestViews(TestCase):      # HOW TO TEST JUST THIS CLASS??? by putting tags decorator and then  : python manage.py test --tag=views
-
-    def setUp(self):
-        self.offerList_url = reverse('offers list') 
-        offer = Offer()
-        
-
+      
     def test_home_page_renders_correct_html(self):
         response = self.client.get(reverse('home'))
         self.assertEquals(response.status_code,200)
@@ -24,3 +19,24 @@ class TestViews(TestCase):      # HOW TO TEST JUST THIS CLASS??? by putting tags
         response = self.client.get(self.offerList_url)
         self.assertEquals(response.status_code,200)
         print(response.json())
+
+@tag('offerviewlist')
+class TestOfferViewList(TestCase):
+
+    def test_get_list_of_offers(self):
+        offer1 = Offer.objects.create(customer_name='test customer')  
+        offer2 = Offer.objects.create(customer_name='test customer 2') 
+        response = self.client.get('/offers/')
+        self.assertEquals(response.status_code,200)
+        self.assertEquals(len(response.json()),2)
+        
+
+    def test_post_new_valid_offer(self):
+        response = self.client.post('/offers/',data={   'customer_name' : 'testcustomer' ,
+                                                        'description' :  "test saving to db with post"})
+        self.assertEqual(Offer.objects.count(), 1)
+        new_offer = Offer.objects.first()
+        self.assertEqual(new_offer.description, 'test saving to db with post')
+
+    # def test_post_new_INvalid_offer(self):
+    #     pass
