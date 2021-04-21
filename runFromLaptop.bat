e0c96a8d9bc35f7be7bc52d76e43b0288fd38be8
Vskouteris <vasilis6103@gmail.com>
2021-04-21 15:28:17 +0300

    configuration from laptop

---
+++ runFromLaptop.bat
@@ -0,0 +1,2 @@
+@echo off
+cmd /k "cd /d C:\Users\user\Documents\Python_Projects\tddDjango\tddenv\Scripts & activate & cd /d    C:\Users\user\Documents\Python_Projects\tddDjango\tddDjango & python manage.py runserver"
